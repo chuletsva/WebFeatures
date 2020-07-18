78805b02fe98143574e9ec70c04691a778eb976f
Vyacheslav Chuletc <chuletsva@gmail.com>
2020-07-18 20:35:43 +0200

    small fix

--- tests/Application.Tests/Common/Helpers/EntitySpecimenBuilder.cs
+++ tests/Application.Tests/Common/Helpers/EntitySpecimenBuilder.cs
@@ -7,26 +7,25 @@ using Domian.Common;
 
 namespace Application.Tests.Common.Helpers
 {
-	internal class EntitySpecimenBuilder : ISpecimenBuilder
-	{
-		public object Create(object request, ISpecimenContext context)
-		{
-			if (!(request is PropertyInfo property)) return new NoSpecimen();
-            
-			if (property.Name == nameof(ISoftDelete.IsDeleted)) return false;
-			
-			if (property.PropertyType.IsSubclassOf(typeof(Entity))
-			 || property.PropertyType.IsGenericType
-			 && property.PropertyType.GetInterfaces()
-				   .Any(x => x.IsGenericType && x.GetGenericTypeDefinition() == typeof(IEnumerable<>))
-			 || property.PropertyType.IsGenericType
-			 && property.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>))
-			{
-				return null;
-			}
+    internal class EntitySpecimenBuilder : ISpecimenBuilder
+    {
+        public object Create(object request, ISpecimenContext context)
+        {
+            if (!(request is PropertyInfo property)) return new NoSpecimen();
 
-			return new NoSpecimen();
-                
-		}
-	}
+            if (property.Name == nameof(ISoftDelete.IsDeleted)) return false;
+
+            if (property.PropertyType.IsSubclassOf(typeof(Entity))
+             || property.PropertyType.GetInterfaces()
+                   .Any(x => x.IsGenericType && x.GetGenericTypeDefinition() == typeof(IEnumerable<>))
+             || property.PropertyType.IsGenericType
+             && property.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>))
+            {
+                return null;
+            }
+
+            return new NoSpecimen();
+
+        }
+    }
 }
