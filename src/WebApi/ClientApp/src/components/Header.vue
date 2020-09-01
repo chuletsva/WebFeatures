acf122532fa0dd522e298a58a3fc675f708c8679
Vyacheslav Chuletc <chuletsva@gmail.com>
2020-09-01 10:03:56 +0200

    added header

---
+++ src/WebApi/ClientApp/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app-header">
+    <article>
+      <section>
+        <router-link to="/">
+          <img src="../assets/logo.png" />
+        </router-link>
+      </section>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+</style>
