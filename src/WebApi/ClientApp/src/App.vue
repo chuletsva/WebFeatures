05ada045e4a148b615cd7509a5955c922f44b660
Vyacheslav Chuletc <chuletsva@gmail.com>
2020-09-03 22:12:46 +0200

    css

--- src/WebApi/ClientApp/src/App.vue
+++ src/WebApi/ClientApp/src/App.vue
@@ -18,5 +18,73 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
+html,
+body {
+  font-family: Montserrat, sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  z-index: 10;
+}
+
+*,
+*::after,
+*::before {
+  box-sizing: border-box;
+}
+
+.preloading {
+  color: #00a69c;
+  display: block;
+  font-size: 1.5rem;
+  left: 50%;
+  position: fixed;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+
+select::-ms-expand {
+  display: none;
+}
+
+@media screen and (min-width: 992px) {
+  .form-input {
+    max-width: 360px;
+    width: 360px;
+  }
+}
+.form-input {
+  border-radius: 0;
+  height: 45px;
+  padding: 10px;
+}
+.form-input-small {
+  max-width: 100px !important;
+}
+.form-input-medium {
+  width: 150px !important;
+}
+.alert {
+  padding-left: 0;
+}
+.alert-danger {
+  background-color: transparent;
+  border: 0;
+  color: #fb0d0d;
+  font-size: 12px;
+}
+a,
+a:active,
+a:hover,
+a:visited {
+  color: #000;
+  text-decoration: none;
+  transition: color 0.35s;
+}
+
+a:hover,
+a:active {
+  color: #75b918;
+  transition: color 0.35s;
+}
 </style>
--- src/WebApi/ClientApp/src/components/Catalog.vue
+++ src/WebApi/ClientApp/src/components/Catalog.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="catalog">
-    <div class="catalog-item" v-for="product in products" :key="product.id">
-      <form>
-        <img
-          class="catalog-item-image"
-          v-bind:src="'https://localhost:5001/api/files/' + product.pictureId"
-          width="400px"
-          height="auto"
-        />
-        <input type="submit" value="[ADD TO BASKET]" />
-        <span>{{ product.name }}</span>
-        <span>{{ product.price }}</span>
-      </form>
+    <div class="catalog-items">
+      <div class="catalog-item" v-for="product in products" :key="product.id">
+        <form>
+          <img
+            class="catalog-item-image"
+            v-bind:src="'https://localhost:5001/api/files/' + product.pictureId"
+            width="400px"
+            height="auto"
+          />
+          <input
+            class="catalog-item-button"
+            type="submit"
+            value="[ADD TO BASKET]"
+          />
+          <div class="catalog-item-name">
+            <span>{{ product.name }}</span>
+          </div>
+          <div class="catalog-item-price">
+            <span>{{ product.price }}</span>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -32,10 +42,65 @@ export default {
 </script>
 
 <style scoped>
-.catalog {
+.catalog-items {
   padding-right: 15px;
   padding-left: 15px;
   margin-right: auto;
   margin-left: auto;
+}
+.catalog-item {
+  margin-bottom: 1.5rem;
+  text-align: center;
+  width: 33%;
+  display: inline-block;
+  float: none !important;
+}
+@media screen and (max-width: 1024px) {
+  .catalog-item {
+    width: 50%;
+  }
+}
+@media screen and (max-width: 768px) {
+  .catalog-item {
+    width: 100%;
+  }
+}
+.catalog-item-image {
+  max-width: 370px;
+  width: 100%;
+}
+.catalog-item-button {
+  background-color: #83d01b;
+  border: 0;
+  color: #ffffff;
+  cursor: pointer;
+  font-size: 1rem;
+  height: 3rem;
+  margin-top: 1rem;
+  transition: all 0.35s;
+  width: 80%;
+}
+.catalog-item-button.is-disabled {
+  opacity: 0.5;
+  pointer-events: none;
+}
+.catalog-item-button :hover {
+  background-color: #4a760f;
+  transition: all 0.35s;
+}
+.catalog-item-name {
+  font-size: 1rem;
+  font-weight: 300;
+  margin-top: 0.5rem;
+  text-align: center;
+  text-transform: uppercase;
+}
+.catalog-item-price {
+  font-size: 28px;
+  font-weight: 900;
+  text-align: center;
+}
+.catalog-item-price::before {
+  content: "$";
 }
 </style>
